ab0be006af960480c1361880ac6649c91e83f108
Ryan Gibbons <rtgibbons23@gmail.com>
2013-02-20 11:27:59 -0700

    initial import of php54 formula from josegonzalez/homebrew-php

---
+++ files/brews/php54-imagick.rb
@@ -0,0 +1,25 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php54Imagick < AbstractPhp54Extension
+  init
+  homepage 'http://pecl.php.net/package/imagick'
+  url 'http://pecl.php.net/get/imagick-3.1.0RC2.tgz'
+  sha1 '29b6dcd534cde6b37ebe3ee5077b71a9eed685c2'
+  head 'https://svn.php.net/repository/pecl/imagick/trunk/', :using => :svn
+
+  depends_on 'pkg-config' => :build
+  depends_on 'imagemagick'
+
+  def install
+    Dir.chdir "imagick-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/imagick.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
